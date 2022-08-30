9d23e7a59741e47c80da6993d6c231afd00a0b1f
Ahmed K. A <ahmadkabdullah@pm.me>
2022-08-30 20:32:18 +0300

    refactor: move dumm data to separate file

--- src/App.vue
+++ src/App.vue
@@ -6,40 +6,14 @@
 <script>
 import SearchBar from "./components/SearchBar.vue";
 import SearchList from "./components/SearchList.vue";
+import { filterData } from "./logic/data.js";
 
 export default {
   name: "App",
   components: { SearchBar, SearchList },
   data() {
     return {
-      days: [
-        {
-          id: 1,
-          type: "command",
-          title: "Delete Day",
-        },
-        {
-          id: 2,
-          type: "day",
-          title: "Performed the play",
-          tags: ["scared", "proud"],
-          date: "19 Sep 2022",
-        },
-        {
-          id: 3,
-          type: "day",
-          title: "Was congratulated for my acting",
-          tags: ["happy", "proud"],
-          date: "20 Sep 2022",
-        },
-        {
-          id: 4,
-          type: "dream",
-          title: "Dreamt of climbing a mountain",
-          tags: ["scared", "excited"],
-          date: "28 Oct 2022",
-        },
-      ],
+      days: filterData(),
     };
   },
   methods: {
---
+++ src/logic/data.js
@@ -0,0 +1,30 @@
+export function filterData() {
+  return [
+    {
+      id: 1,
+      type: "command",
+      title: "Delete Day",
+    },
+    {
+      id: 2,
+      type: "day",
+      title: "Performed the play",
+      tags: ["scared", "proud"],
+      date: "19 Sep 2022",
+    },
+    {
+      id: 3,
+      type: "day",
+      title: "Was congratulated for my acting",
+      tags: ["happy", "proud"],
+      date: "20 Sep 2022",
+    },
+    {
+      id: 4,
+      type: "dream",
+      title: "Dreamt of climbing a mountain",
+      tags: ["scared", "excited"],
+      date: "28 Oct 2022",
+    },
+  ];
+}
