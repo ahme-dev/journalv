51e06e5d7dae4312b4177ef52d81212794d2fcf4
Ahmed K. A <ahmadkabdullah@pm.me>
2022-08-30 21:16:35 +0300

    feat: ids are assigned on search so first item will always have id of 1

--- src/logic/data.js
+++ src/logic/data.js
@@ -1,6 +1,5 @@
 const startData = [
   {
-    id: 1,
     type: "command",
     title: "Nothing to show here!",
   },
@@ -8,26 +7,22 @@ const startData = [
 
 const dummyData = [
   {
-    id: 1,
     type: "command",
     title: "Delete Day",
   },
   {
-    id: 2,
     type: "day",
     title: "Performed the play",
     tags: ["scared", "proud"],
     date: "19 Sep 2022",
   },
   {
-    id: 3,
     type: "day",
     title: "Was congratulated for my acting",
     tags: ["happy", "proud"],
     date: "20 Sep 2022",
   },
   {
-    id: 4,
     type: "dream",
     title: "Dreamt of climbing a mountain",
     tags: ["scared", "excited"],
@@ -36,17 +31,22 @@ const dummyData = [
 ];
 
 export function filterData(searchPhrase) {
+  let counter = 0;
   const newData = dummyData.filter((entry) => {
     // if no search phrase was entered deny filtering
     if (searchPhrase === "") return false;
 
     if (entry["title"].toLowerCase().includes(searchPhrase.toLowerCase())) {
+      counter += 1;
+      entry.id = counter;
       return true;
     }
   });
 
   // if result was empty
   if (newData.toString() == "") return startData;
+
+  console.log(newData);
 
   return newData;
 }
