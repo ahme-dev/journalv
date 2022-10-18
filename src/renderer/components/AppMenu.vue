0ee0d33b6572d7d7691fad3b8660461a2f246834
Ahmed K. A <ahmadkabdullah@pm.me>
2022-10-18 18:51:28 +0300

    style: add styling of searchlist to appmenu

--- src/renderer/components/AppMenu.vue
+++ src/renderer/components/AppMenu.vue
@@ -9,12 +9,24 @@
 <style scoped>
   .AppMenu {
     display: flex;
-    align-items: flex-end;
+    flex-direction: column-reverse;
+    justify-content: flex-start;
 
+    padding: 3rem;
     gap: 1rem;
-    padding: 1rem;
 
     border-top-left-radius: 1rem;
+    border-top-right-radius: 1rem;
+
+    overflow-y: scroll;
+  }
+  .AppMenu::-webkit-scrollbar {
+    background-color: var(--main);
+    border-top-right-radius: 1rem;
+    overflow: hidden;
+  }
+  .AppMenu::-webkit-scrollbar-thumb {
+    background-color: var(--accent);
     border-top-right-radius: 1rem;
   }
 </style>
