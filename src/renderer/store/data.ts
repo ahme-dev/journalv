e594f1a54db06b616bd0871abadb7314e183f502
Ahmed K. A <ahmadkabdullah@pm.me>
2022-10-29 15:27:34 +0300

    refactor: add theme to app data structure

--- src/renderer/store/data.ts
+++ src/renderer/store/data.ts
@@ -8,46 +8,57 @@ export interface Entry {
   id: number;
 }
 
-// dummy data
-let data: Entry[] = [
-  {
-    type: "day",
-    title: "Performed the play",
-    tags: ["scared", "proud"],
-    content: "",
-    date: "19 Sep 2022",
-    id: 221,
-  },
-  {
-    type: "day",
-    title: "Was congratulated for my acting",
-    tags: ["happy", "proud"],
-    date: "20 Sep 2022",
-    content: "",
-    id: 222,
-  },
-  {
-    type: "dream",
-    title: "Dreamt of climbing a mountain",
-    tags: ["scared", "excited"],
-    content: "",
-    date: "28 Oct 2022",
-    id: 223,
-  },
-];
+// type for app data
+export interface AppData {
+  theme: string,
+  entries: Entry[],
+}
+
+// APP data
+
+let app: AppData = {
+  theme: 'orange',
+  // temp dummy entries
+  entries: [
+    {
+      type: "day",
+      title: "Performed the play",
+      tags: ["scared", "proud"],
+      content: "",
+      date: "19 Sep 2022",
+      id: 221,
+    },
+    {
+      type: "day",
+      title: "Was congratulated for my acting",
+      tags: ["happy", "proud"],
+      date: "20 Sep 2022",
+      content: "",
+      id: 222,
+    },
+    {
+      type: "dream",
+      title: "Dreamt of climbing a mountain",
+      tags: ["scared", "excited"],
+      content: "",
+      date: "28 Oct 2022",
+      id: 223,
+    },
+  ]
+}
 
 // DATA functions
 
 // call preload function
 // to export data to file
 export const exportData = () => {
-  window.backend.writeJournal(data);
+  window.backend.writeJournal(app.entries);
 }
 
 // call preload function
 // to import data from file
 export const importData = () => {
-  data = window.backend.readJournal()
+  app.entries = window.backend.readJournal()
 }
 
 // CRUD interface high-order functions for the data
@@ -55,7 +66,7 @@ export const importData = () => {
 export function createEntry(type: "day" | "dream", date: string): number {
   // create an entry type from parameters
   let newEntry: Entry = {
-    id: data[data.length - 1].id + 1,
+    id: app.entries[app.entries.length - 1].id + 1,
     title: "",
     content: "",
     type: type,
@@ -64,14 +75,14 @@ export function createEntry(type: "day" | "dream", date: string): number {
   };
 
   // add entry to data list
-  data.push(newEntry);
+  app.entries.push(newEntry);
 
   // return the id of the created entry
   return newEntry.id;
 }
 
 export function readEntry(filterFunc: (entry: Entry) => boolean): Entry[] {
-  return data.filter(filterFunc);
+  return app.entries.filter(filterFunc);
 }
 
 export function updateEntry(
@@ -80,7 +91,7 @@ export function updateEntry(
   newContent: string,
   newTags: string[]
 ): void {
-  data.map((element) => {
+  app.entries.map((element) => {
     if (element.id === id) {
       element.title = newTitle;
       element.content = newContent;
