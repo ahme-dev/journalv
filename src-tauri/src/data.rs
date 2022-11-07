9b9394378b364ac52e8031fcc5f42c45bcf808cc
Ahmed K. A <ahmadkabdullah@pm.me>
2022-11-07 09:58:11 +0300

    refactor: change rust indent to tabs

--- src-tauri/src/data.rs
+++ src-tauri/src/data.rs
@@ -1,26 +1,23 @@
-use std::{
-    fs,
-    path,
-};
+use std::{fs, path};
 
 fn get_path() -> path::PathBuf {
-    tauri::api::path::config_dir()
-        .expect("could not get config dir")
-        .join("journalv.data")
+	tauri::api::path::config_dir()
+		.expect("could not get config dir")
+		.join("journalv.data")
 }
 
 #[tauri::command]
 pub fn write_data(data: String) {
-    let path = get_path();
+	let path = get_path();
 
-    // write the passed data into file using path
-    fs::write(path, data).expect("could not write file")
+	// write the passed data into file using path
+	fs::write(path, data).expect("could not write file")
 }
 
 #[tauri::command]
 pub fn read_data() -> String {
-    let path = get_path();
+	let path = get_path();
 
-    // read file, using path, into a string and return
-    fs::read_to_string(path).expect("could not read file")
+	// read file, using path, into a string and return
+	fs::read_to_string(path).expect("could not read file")
 }
--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,13 +1,13 @@
 #![cfg_attr(
-    all(not(debug_assertions), target_os = "windows"),
-    windows_subsystem = "windows"
+	all(not(debug_assertions), target_os = "windows"),
+	windows_subsystem = "windows"
 )]
 
 mod data;
 
 fn main() {
-    tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![data::write_data, data::read_data])
-        .run(tauri::generate_context!())
-        .expect("error while running tauri application");
+	tauri::Builder::default()
+		.invoke_handler(tauri::generate_handler![data::write_data, data::read_data])
+		.run(tauri::generate_context!())
+		.expect("error while running tauri application");
 }
